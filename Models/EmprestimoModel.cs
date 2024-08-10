999add90e7041c04d6fcb3df7c82dae917a9ec69
Jose Otavio dos Santos Miguel <joseotavio_miguel@outlook.com>
2024-08-10 19:31:34 -0300

    Primeiro Commit

---
+++ Models/EmprestimoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmprestimoLivros.Models
+{
+    // Estabelecendo o cabeçalho da Tabela
+    public class EmprestimoModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+    }
+}
